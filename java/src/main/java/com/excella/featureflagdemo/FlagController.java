a29cc14e6c2dd110a12add3ef60b30833dbc85db
ephultman <mister.parke@gmail.com>
2020-08-03 11:47:34 -0400

    Removed Blocking

--- java/src/main/java/com/excella/featureflagdemo/FlagController.java
+++ java/src/main/java/com/excella/featureflagdemo/FlagController.java
@@ -3,6 +3,7 @@ package com.excella.featureflagdemo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class FlagController {
@@ -13,12 +14,11 @@ public class FlagController {
     webClient = WebClient.create("http://localhost:18000/api/v1");
   }
 
-  @GetMapping("/allFa")
-  public String obtainAllFlags() {
+  @GetMapping("/allFlags")
+  public Mono<String> obtainAllFlags() {
     return webClient
-        .get().uri("flags").exchange()
-        .flatMap(resp -> resp.bodyToMono(String.class))
-        .block();
+        .get().uri("/flags").exchange()
+        .flatMap(resp -> resp.bodyToMono(String.class));
   }
 
 }
