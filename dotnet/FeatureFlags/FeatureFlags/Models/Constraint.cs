61ffc63d57ccad45274deef2a2fc6068079808d5
Waleed <waleedershad@gmail.com>
2020-08-04 14:44:33 -0400

    Created .NET Core Web App project along with models

---
+++ dotnet/FeatureFlags/FeatureFlags/Models/Constraint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeatureFlags.Models
+{
+    public class Constraint
+    {
+        public int Id { get; set; }
+
+        public string Property { get; set; }
+
+        public string Operator { get; set; }
+
+        public string Value { get; set; }
+    }
+}
