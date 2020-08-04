c826552d39ebd05fc7b94dceab69efcb97395757
khoi <khoile0727@gmail.com>
2020-08-04 16:14:01 -0400

    clean up unused routes

--- ruby/config/routes.rb
+++ ruby/config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  get 'flagr/index'
-  get 'flagr/find_flags'
-  get '/api/v1/flags' => 'flagr#find_flags'
   get 'flagr/health'
-  get '/api/v1/health' => 'flagr#health'
-
-
-  post 'flagr/create_flag'
-
-  post '/api/v1/flags' => 'flagr#create_flag'
-  # post '/flags' => 'flager#create_flag'
 
   root 'flagr#index'
   resources :todos do
