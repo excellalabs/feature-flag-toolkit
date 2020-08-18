62fbf9c7acf08f980a8e862366c9790c9dca4553
Devin Venetsanos <devinv@vt.edu>
2020-08-18 11:36:06 -0400

    Show users flags

--- python/app/models.py
+++ python/app/models.py
@@ -8,3 +8,21 @@ class User(db.Model):
   username = db.Column(db.String(50))
   password = db.Column(db.String(20))
   role = db.Column(db.String(20))
+
+  @property
+  def is_active(self):
+    return True
+
+  @property
+  def is_authenticated(self):
+    return True
+
+  @property
+  def is_anonymous(self):
+    return False
+
+  def get_id(self):
+        try:
+            return self.id
+        except AttributeError:
+            raise NotImplementedError('No `id` attribute - override `get_id`')
--- python/app/views.py
+++ python/app/views.py
@@ -3,33 +3,28 @@ import time
 import flagr
 from flagr.rest import ApiException
 from flask import render_template, request, flash, redirect, url_for
+from flask_login import logout_user, current_user, login_user
 from app.forms import LoginForm, CreateUserForm
 from app.models import User, db
 
+FLAGR_BASE_URL = "python_flagr_1:18000/api/v1"
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
     if request.method == 'POST' and form.validate():
         user = User.query.filter(User.username == form.username.data).first()
+        login_user(user)
         if (user.password == form.password.data):
-            return redirect(url_for(display_all_flags))
+            return redirect(url_for('get_users_flags'))
     return render_template('login.html', form=form)
 
 @app.route('/flags')
 def display_all_flags():
-  # create an instance of the API class
-  api_instance = flagr.FlagApi()
-  api_instance.api_client.configuration.host = "python_flagr_1:18000/api/v1"
+    flags = get_all_flags()
+    return render_template('display_flags.html', all_flags=flags)
 
-  try:
-      api_response = api_instance.find_flags()
-      print(api_response)
-      return render_template('display_flags.html', all_flags=api_response)
-  except ApiException as e:
-      return render_template('error.html', error=e)
-
-@app.route('/new-user', methods=['GET', 'POST'])
+@app.route('/user/create', methods=['GET', 'POST'])
 def create_user():
     form = CreateUserForm(request.form)
     if request.method == 'POST' and form.validate():
@@ -37,6 +32,51 @@ def create_user():
         new_account = User(username=form.username.data, password=form.password.data, role=form.role.data)
         db.session.add(new_account)
         db.session.commit()
-        return redirect(url_for('display_all_flags'))
+        login_user(new_account)
+        return redirect(url_for('get_users_flags'))
     return render_template('create_user.html', form=form)
 
+@app.route('/user/flags')
+def get_users_flags():
+    flags = get_all_flags()
+    eval_api_instance = flagr.EvaluationApi()
+    eval_api_instance.api_client.configuration.host = FLAGR_BASE_URL
+    # flagr.EvalContext returns this empty json object 
+    # {
+    #   'eval_context': 
+    #     {
+    #       'enable_debug': None, 
+    #       'entity_context': None, 
+    #       'entity_id': None, 
+    #       'entity_type': None, 
+    #       'flag_id': None, 
+    #       'flag_key': None, 
+    #       'flag_tags': None
+    #     }
+    # }
+    body = flagr.EvalContext()
+    body.entity_context = { "role": current_user.role }
+
+    user_flags = []
+
+    for flag in flags:
+        try:
+            body.flag_id = flag.id
+            api_response = eval_api_instance.post_evaluation(body)
+            if api_response.variant_key != 'Off':
+                user_flags.append(flag)
+        except ApiException as e:
+            return render_template('error.html', error=e)
+    
+    return render_template('display_flags.html', all_flags=user_flags)
+
+
+def get_all_flags():
+    api_instance = flagr.FlagApi()
+    api_instance.api_client.configuration.host = FLAGR_BASE_URL
+
+    try: 
+        api_response = api_instance.find_flags()
+        return api_response
+    except ApiException as e:
+        return render_template('error.html', error=e)
